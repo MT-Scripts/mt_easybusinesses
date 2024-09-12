a4c755c2c07819411918df4bf5cfdc674cbb8069
Marttins <89866234+Marttins011@users.noreply.github.com>
2024-09-12 12:19:26 +0100

    make: Documentation params

--- server/main.lua
+++ server/main.lua
@@ -1,15 +1,35 @@
+---@param source number
+---@param id string | number
+---@param label string
+---@param slots number
+---@param weight number
 lib.callback.register('mt_easybusinesses:server:registerStash', function(source, id, label, slots, weight)
     return exports.ox_inventory:RegisterStash(id, label, slots, weight)
 end)
 
+---@param source number
+---@param id string | number
+---@param items table
+---@param label string
 lib.callback.register('mt_easybusinesses:server:registerCraft', function(source, id, items, label)
     return exports.ox_inventory:RegisterCraftStation(id, { items = items, label = label })
 end)
 
+---@param source number
+---@param id string | number
+---@param label string
+---@param items table
 lib.callback.register('mt_easybusinesses:server:registerShop', function(source, id, label, items)
     return exports.ox_inventory:RegisterShop(id, { name = label, inventory = items })
 end)
 
+---@param source number
+---@param id string | number
+---@param label string
+---@param slots number
+---@param weight number
+---@param job string
+---@param grade number
 lib.callback.register('mt_easybusinesses:server:registerShowcaseStash', function(source, id, label, slots, weight, job, grade)
     exports.ox_inventory:RegisterStash(id, label, slots, weight)
 
